be9da795043645a3faee86802cc8bb67f0ec2295
Jaeseok Heo <jasonheo1998@gmail.com>
2025-04-21 14:05:51 +0900

    Init: Spring Boot 프로젝트 전체 파일 추가

---
+++ src/test/java/com/hrbank/Sb02HrbankTeam5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb02HrbankTeam5ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
