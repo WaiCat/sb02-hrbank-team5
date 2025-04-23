94090f92e25d47f8d83dae958cec5608a4a4c384
urur-27 <73218039+urur-27@users.noreply.github.com>
2025-04-23 14:18:48 +0900

    [#47] Mapper 오류 개선 및 자동화 (#48)
    * feat: 직원 이력 도메인 정의 및 저장 구조 설계 #7
    
    - employeeNumber, memo, ipAddress 등 필터를 유연하게 조합하기 위해 Specification 사용
    
    * refactor: 엔티티 ID 타입을 UUID → Long으로 변경하여 DB 설계와 일치 #7
    
    * refactor: PR 리뷰 반영 - 컬럼 이름과 타입 정의 추가로 스키마 정합성 확보, setter 제거 및 관계 설정 로직 보안
    
    * refactor: PR 리뷰 반영 - table명 명시적으로 지정
    
    * feat: 직원 이력 로그 저장 기능 구현 완료 (#14)
    
    - 직원 생성/수정/삭제 구분 로직 정의
    - 변경 필드 추출 및 상세 로그(EmployeeChangeLogDetail) 저장
    - 메모는 파라미터, IP는 요청에서 추출
    - 중복 비교 제거를 위한 addDetailIfChanged(...) 리팩토링 적용
    
    * feat: 기준 시각 이후 변경 이력 존재 여부 확인 API 구현 (#25)
    
    * feat: 변경 이력 저장 기능의 기반 DTO record 적용 및 서비스 로직 일부 정리 (#25)
    
    - 기존 EmployeeChangeLogSearchRequest 클래스를 record로 리팩토링
    - 목록/상세/페이지네이션 응답용 기반 DTO (DiffDto, ChangeLogDto, CursorPageResponseChangeLogDto) 추가
    - EmployeeChangeLogMapper 구성
    - BasicEmployeeChangeLogService 내 변경 DTO 구조에 맞춰 로직 일부 정리
    
    * feat: 직원 정보 수정 이력 목록 조회 API 구현 (cursor 기반 페이지네이션 적용) #30
    
    - 사번, 메모, IP주소, 유형, 기간으로 필터링 가능한 이력 목록 조회 API 구현
    - 커서 기반 페이지네이션 적용
    - 목록 응답에 details 필드 제거 (상세 API에서 별도 조회 예정)
    
    * fix: 누락된 변경 내용 추가
    
    * fix: 가독형 향상을 위해 표기 방식 변경 "A!=true"가 아닌 "!A"표현 사용
    
    * feat: 수동 Mapper를 MapStruct 기반 자동 Mapper로 전환

--- src/main/java/com/hrbank/mapper/EmployeeChangeLogMapper.java
+++ src/main/java/com/hrbank/mapper/EmployeeChangeLogMapper.java
@@ -4,34 +4,10 @@ import com.hrbank.dto.employeeChangeLog.ChangeLogDto;
 import com.hrbank.dto.employeeChangeLog.DiffDto;
 import com.hrbank.entity.EmployeeChangeLog;
 import com.hrbank.entity.EmployeeChangeLogDetail;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class EmployeeChangeLogMapper {
-
-  public ChangeLogDto toDto(EmployeeChangeLog entity) {
-    List<DiffDto> details = entity.getDetails()
-        .stream()
-        .map(this::toDiffDto)
-        .collect(Collectors.toList());
-
-    return new ChangeLogDto(
-        entity.getId(),
-        entity.getType(),
-        entity.getEmployeeNumber(),
-        entity.getMemo(),
-        entity.getIpAddress(),
-        entity.getAt()
-    );
-  }
-
-  public DiffDto toDiffDto(EmployeeChangeLogDetail detail) {
-    return new DiffDto(
-        detail.getPropertyName(),
-        detail.getBefore(),
-        detail.getAfter()
-    );
-  }
+@Mapper(componentModel = "spring")
+public interface EmployeeChangeLogMapper {
+  ChangeLogDto toDto(EmployeeChangeLog entity);
+  DiffDto toDiffDto(EmployeeChangeLogDetail detail);
 }
